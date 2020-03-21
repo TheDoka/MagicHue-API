fd6f083c2f72522a783490c11ee60ea159938e67
TheDoka <ytmag@hotmail.fr>
2020-03-21 06:12:39 +0100

    Add files via upload

---
+++ MagicHue - Controler/MagicHue - Controler/Settings.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace MagicHue___Controler
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
+            this.mailBox = new System.Windows.Forms.TextBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mailBox
+            // 
+            this.mailBox.Location = new System.Drawing.Point(6, 32);
+            this.mailBox.Name = "mailBox";
+            this.mailBox.Size = new System.Drawing.Size(198, 20);
+            this.mailBox.TabIndex = 0;
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(6, 71);
+            this.passBox.Name = "passBox";
+            this.passBox.PasswordChar = '*';
+            this.passBox.Size = new System.Drawing.Size(198, 20);
+            this.passBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mail";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(123, 118);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(84, 23);
+            this.LoginButton.TabIndex = 1;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.mailBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.passBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(219, 102);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Info";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(33, 118);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(84, 23);
+            this.CloseButton.TabIndex = 6;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 149);
+            this.ControlBox = false;
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.LoginButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Settings";
+            this.Text = "Settings";
+            this.Load += new System.EventHandler(this.Settings_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mailBox;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button CloseButton;
+    }
+}
---
+++ MagicHue - Controler/MagicHue - Controler/Settings.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MagicHue___Controler
+{
+    public partial class Settings : Form
+    {
+
+        public MagicHueAPI Instance = new MagicHueAPI();
+        static Form1 FormOne;
+
+        public Settings(Form1 frm)
+        {
+            InitializeComponent();
+            FormOne = frm;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (LoginButton.Text == "Login")
+            { 
+
+                if ( Instance.Login(mailBox.Text, passBox.Text) )
+                {
+                    MessageBox.Show("Logged in!", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    FormOne.listView1.Items.Clear();
+                    FormOne.FillWith(Instance.GetAssociatedDevices());
+
+                    FormOne.listView1.Enabled = true;
+                    if (FormOne.listView1.Items.Count > 0)
+                    {
+                        FormOne.listView1.Items[0].Selected = true;
+                        FormOne.listView1.Select();
+                        FormOne.listView1.EnsureVisible(0);
+                    }
+                    FormOne.toolStripMenuItem1.Enabled = true;
+                    FormOne.turnOnToolStripMenuItem1.Enabled = true;
+                    FormOne.turnOffToolStripMenuItem1.Enabled = true;
+
+                    FormOne.toolStripSplitButton1.Enabled = true;
+
+                    FormOne.toolStripStatusLabel1.Text = "Logged in.";
+                    FormOne.Instance = Instance;
+                    LoginButton.Text = "Disconnect";
+                    mailBox.Enabled = false;
+                    passBox.Enabled = false;
+                    this.Hide();
+
+                } else {
+                    MessageBox.Show("Login failed, please try again.", "Login", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+
+            }
+            else {
+
+                LoginButton.Text = "Login";
+                mailBox.Enabled = true;
+                passBox.Enabled = true;
+                FormOne.listView1.Enabled = false;
+                FormOne.contextMenuStrip2.Enabled = false;
+                FormOne.toolStripMenuItem1.Enabled = false;
+                FormOne.turnOnToolStripMenuItem1.Enabled = false;
+                FormOne.turnOffToolStripMenuItem1.Enabled = false;
+                FormOne.toolStripSplitButton1.Enabled = false;
+
+                FormOne.toolStripStatusLabel1.Text = "Not logged in, please login.";
+            }
+            
+        }
+
+        private void Settings_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
